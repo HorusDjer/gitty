8700e42270fe551cd9e15f396c4cabd4d5d75b0e
HorusDjer <eobineme@gmail.com>
2018-06-14 12:02:09 -0400

    cleaned query

---
+++ total_attempts.sql
@@ -0,0 +1,10 @@
+SELECT
+ statecode
+ ,COUNT(VAN_ccvf.ResultID) AS total_conversations
+FROM fof_vansync.contacts_contacts_vf VAN_ccvf
+LEFT JOIN fof_vansync.results VAN_r on VAN_ccvf.ResultID = VAN_r.ResultID
+LEFT JOIN fof_vansync.contact_types VAN_ct on VAN_ccvf.ContactTypeID = VAN_ct.ContactTypeID
+WHERE VAN_ccvf.DateCanvassed >= '2018-01-01'
+ AND VAN_ccvf.ContactTypeID = 2 --walk
+ AND VAN_r.ResultID = 14 --find total conversations
+GROUP BY statecode
